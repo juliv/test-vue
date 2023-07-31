3c333c96ab35628d8446b8da73587a9cfd7ce829
juliv <julivgan@gmail.com>
2023-07-31 22:54:37 +0400

    ci: Настроить окружение

---
+++ src/components/StartBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="start-box">
+    <h1>{{ title }}</h1>
+    <div>
+      <img alt="Vue logo" src="@/assets/logo.png">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    title: String,
+  },
+})
+
+export default class StartBox extends Vue {
+  title!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <StartBox title="Flame Тестовое фронт"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import StartBox from '@/components/StartBox.vue';
+
+@Options({
+  components: {
+    StartBox,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
